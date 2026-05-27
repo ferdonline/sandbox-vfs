112d118983b563c9f3ac93b6f54b73d797601f38
Fernando Pereira <ferdonline@gmail.com>
2026-05-27 23:43:40 +0100

    Add some more tests to memoryFS

--- src/impls/memory.rs
+++ src/impls/memory.rs
@@ -16,7 +16,7 @@ use std::{
 
 // use append_only_vec::AppendOnlyVec;
 
-use libc::{c_void, mode_t};
+use libc::{c_void, mode_t, O_CREAT};
 
 use crate::filesystem::LowLevelFS;
 
@@ -47,7 +47,7 @@ pub struct MemFsEntry {
     kind: FileKind,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum FileKind {
     File,
     Dir,
@@ -76,12 +76,20 @@ impl MemFsEntry {
 
 impl MemoryFS {
     pub fn new(id: impl Into<String>) -> Box<Self> {
+        let root = Arc::new(MemFsEntry::new(
+            create_memfd(b"root\0"),
+            PathBuf::from("/"),
+            FileKind::Dir,
+        ));
+        let mut fs = HashMap::new();
+        fs.insert(PathBuf::from("/"), root);
+
         // let base_fd = ALL_MEM_FS.len() * super::MEMORY_FD_START;
         let memfs = Box::new(Self {
             id: id.into(),
             base_fd: 0,
             cur_dir_fd: 0,
-            fs: RwLock::new(HashMap::new()),
+            fs: RwLock::new(fs),
         });
         // Hack: In order to avoid changing API everywhere to support a non-case, we keep static refs to the created mem filesystems
         // This is fine as long as we agree that filesystems are only destroyed at the end
@@ -92,6 +100,55 @@ impl MemoryFS {
     // This intercepted call is unique to memory-fs
     pub fn getdents64(&self, fd: i32, dirp: *mut c_void, count: i32) -> isize {
         todo!()
+    }
+
+    fn parent_is_dir(&self, path: &Path) -> bool {
+        path.parent().is_some_and(|parent| {
+            self.fs
+                .read()
+                .unwrap()
+                .get(parent)
+                .is_some_and(|entry| entry.kind == FileKind::Dir)
+        })
+    }
+
+    fn memfd_name(path: &Path) -> Option<Vec<u8>> {
+        let parent = path.parent()?;
+        let file_name = path.file_name()?;
+        let parent_hash = calculate_hash_seq(parent.as_os_str().as_bytes());
+        Some(format!("{parent_hash:x}_{}\0", file_name.to_string_lossy()).into_bytes())
+    }
+
+    fn create_entry(&self, path: &Path, kind: FileKind) -> i32 {
+        if !path.is_absolute() || path == Path::new("/") || !self.parent_is_dir(path) {
+            return -1;
+        }
+
+        let mut fs = self.fs.write().unwrap();
+        if fs.contains_key(path) {
+            return -1;
+        }
+
+        let Some(name) = Self::memfd_name(path) else {
+            return -1;
+        };
+        let fd = create_memfd(&name);
+        if fd < 0 {
+            return -1;
+        }
+        #[cfg(test)]
+        println!(
+            "pid: {}, memfd: {}, name: {}",
+            std::process::id(),
+            fd,
+            String::from_utf8_lossy(&name)
+        );
+
+        fs.insert(
+            path.into(),
+            Arc::new(MemFsEntry::new(fd, path.into(), kind)),
+        );
+        0
     }
 }
 
@@ -108,37 +165,32 @@ impl LowLevelFS for MemoryFS {
         }
     }
 
-    fn open(&self, path: &std::path::Path, _oflag: i32, _mode: mode_t) -> RawFd {
+    fn open(&self, path: &std::path::Path, oflag: i32, _mode: mode_t) -> RawFd {
         if let Some(memfile) = self.fs.read().unwrap().get(path) {
             return memfile.fd;
         }
-        -1
+
+        if oflag & O_CREAT == 0 || self.create_entry(path, FileKind::File) != 0 {
+            return -1;
+        }
+
+        self.fs
+            .read()
+            .unwrap()
+            .get(path)
+            .map_or(-1, |memfile| memfile.fd)
     }
 
     // Function to create a directory (mkdir)
     fn mkdir(&self, path: &Path, _mode: mode_t) -> RawFd {
-        let parent = path.parent().unwrap();
-        let parent_hash = calculate_hash_seq(parent.as_os_str().as_bytes());
-        let name = format!(
-            "{:x}_{}\0",
-            parent_hash,
-            path.file_name().unwrap().to_string_lossy()
-        );
-        let fd = create_memfd(name.as_bytes());
-        if fd < 0 {
-            return -1;
-        }
-        #[cfg(test)]
-        println!("pid: {}, memfd: {}, name: {}", std::process::id(), fd, name);
-
-        // Add directory entry to VFS
-        let fs_entry = Arc::new(MemFsEntry::new(fd, path.into(), FileKind::Dir));
-        self.fs.write().unwrap().insert(path.into(), fs_entry);
-        return 0; // success
+        self.create_entry(path, FileKind::Dir)
     }
 
-    fn chmod(&self, _path: &Path, _mode: mode_t) -> i32 {
-        return 0; // success
+    fn chmod(&self, path: &Path, _mode: mode_t) -> i32 {
+        match self.fs.read().unwrap().contains_key(path) {
+            true => 0,
+            false => -1,
+        }
     }
 
     fn openat(&self, dirfd: i32, path: &Path, flag: i32, mode: mode_t) -> i32 {
@@ -156,7 +208,7 @@ fn calculate_hash_seq<T: std::hash::Hash>(t: &[T]) -> u64 {
 
 #[cfg(test)]
 mod test {
-    use libc::F_OK;
+    use libc::{F_OK, O_CREAT, O_RDONLY};
 
     use super::*;
 
@@ -178,9 +230,70 @@ mod test {
         assert_ne!(fs.access(test_path, F_OK), 0);
 
         // .access alright after creating it
+        assert_eq!(fs.mkdir(Path::new("/usr"), 0), 0);
+        assert_eq!(fs.mkdir(Path::new("/usr/bin"), 0), 0);
         assert_eq!(fs.mkdir(test_path, 0), 0);
         println!("fs contains: {:?}", fs);
 
         assert_eq!(fs.access(test_path, F_OK), 0);
+    }
+
+    #[test]
+    fn test_root_exists_by_default() {
+        let fs = MemoryFS::new("");
+
+        assert_eq!(fs.access(Path::new("/"), F_OK), 0);
+    }
+
+    #[test]
+    fn test_mkdir_requires_existing_parent() {
+        let fs = MemoryFS::new("");
+
+        assert_ne!(fs.mkdir(Path::new("/usr/bin"), 0), 0);
+        assert_ne!(fs.access(Path::new("/usr/bin"), F_OK), 0);
+    }
+
+    #[test]
+    fn test_mkdir_rejects_duplicate_path() {
+        let fs = MemoryFS::new("");
+
+        assert_eq!(fs.mkdir(Path::new("/usr"), 0), 0);
+        assert_ne!(fs.mkdir(Path::new("/usr"), 0), 0);
+    }
+
+    #[test]
+    fn test_open_without_create_fails_for_missing_file() {
+        let fs = MemoryFS::new("");
+
+        assert_ne!(fs.open(Path::new("/hello.txt"), O_RDONLY, 0), 0);
+    }
+
+    #[test]
+    fn test_open_create_creates_regular_file() {
+        let fs = MemoryFS::new("");
+
+        let fd = fs.open(Path::new("/hello.txt"), O_CREAT, 0o644);
+
+        assert!(fd > 0);
+        assert_eq!(fs.access(Path::new("/hello.txt"), F_OK), 0);
+        assert_eq!(
+            fs.fs.read().unwrap().get(Path::new("/hello.txt")).unwrap().kind,
+            FileKind::File
+        );
+    }
+
+    #[test]
+    fn test_open_create_requires_existing_parent() {
+        let fs = MemoryFS::new("");
+
+        assert_ne!(fs.open(Path::new("/missing/hello.txt"), O_CREAT, 0o644), 0);
+        assert_ne!(fs.access(Path::new("/missing/hello.txt"), F_OK), 0);
+    }
+
+    #[test]
+    fn test_chmod_fails_for_missing_path() {
+        let fs = MemoryFS::new("");
+
+        assert_ne!(fs.chmod(Path::new("/missing"), 0o755), 0);
     }
 }
