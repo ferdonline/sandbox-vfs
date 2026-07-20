e22e7c43b5b811b14a0de52e0f2ec94c7772a41c
Fernando Pereira <ferdonline@gmail.com>
2026-07-20 23:44:48 +0100

    Expand preload smoke coverage

--- tests/preload_smoke.sh
+++ tests/preload_smoke.sh
@@ -50,9 +50,20 @@ SANDBOX_VFS_UPPER="${upper}" \
 SANDBOX_VFS_MEMORY_MOUNT="/memFS" \
 LD_PRELOAD="${preload}" \
 python3 -c '
+import ctypes
 import os
 
 mem_fs = os.environ["SANDBOX_VFS_MEMORY_MOUNT"]
+libc = ctypes.CDLL(None, use_errno=True)
+
+def check(result):
+    if result < 0:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno))
+    return result
+
+def cpath(path):
+    return os.fsencode(path)
 
 fd = os.open(os.path.join(mem_fs, "file.txt"), os.O_CREAT | os.O_WRONLY, 0o644)
 os.close(fd)
@@ -60,4 +71,31 @@ os.mkdir(os.path.join(mem_fs, "subdir"))
 
 entries = sorted(os.listdir(mem_fs))
 assert entries == ["file.txt", "subdir"], entries
+
+dir_fd = check(libc.open(cpath(mem_fs), os.O_RDONLY | os.O_DIRECTORY, 0))
+dup_fd = check(libc.dup(dir_fd))
+fd = check(libc.openat(dup_fd, b"via_dup.txt", os.O_CREAT | os.O_WRONLY, 0o644))
+check(libc.write(fd, b"dup fd", 6))
+check(libc.close(fd))
+check(libc.close(dir_fd))
+check(libc.close(dup_fd))
+
+os.mkdir(os.path.join(mem_fs, "work"))
+os.mkdir(os.path.join(mem_fs, "work", "build"))
+build_fd = check(libc.open(cpath(os.path.join(mem_fs, "work", "build")), os.O_RDONLY | os.O_DIRECTORY, 0))
+check(libc.rename(cpath(os.path.join(mem_fs, "work")), cpath(os.path.join(mem_fs, "renamed"))))
+fd = check(libc.openat(build_fd, b"../from_dotdot.txt", os.O_CREAT | os.O_WRONLY, 0o644))
+check(libc.write(fd, b"renamed parent", 14))
+check(libc.close(fd))
+check(libc.close(build_fd))
+
+assert sorted(os.listdir(mem_fs)) == ["file.txt", "renamed", "subdir", "via_dup.txt"]
+assert sorted(os.listdir(os.path.join(mem_fs, "renamed"))) == ["build", "from_dotdot.txt"]
+
+os.unlink(os.path.join(mem_fs, "via_dup.txt"))
+os.unlink(os.path.join(mem_fs, "renamed", "from_dotdot.txt"))
+os.rmdir(os.path.join(mem_fs, "renamed", "build"))
+os.rmdir(os.path.join(mem_fs, "renamed"))
+
+assert sorted(os.listdir(mem_fs)) == ["file.txt", "subdir"]
 '
